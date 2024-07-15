a233dfa7d2a16f8079862f0969a2972d8b38e702
Ondrej Merta <mertaondrej01@gmail.com>
2024-07-15 15:01:15 +0200

    Refactor authentication routes and add signup page

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,10 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/signup', function () { //route pro Signup Page !!!!!!
+    return Inertia::render('Signup');
+})->name('signup');
 
 //Route::apiResource('members', MemberController::class);
 
@@ -24,7 +25,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-   
+    
+    Route::get('/dashboard', function () { //route pro Admin Panel !!!!!!
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+
 });
 
 require __DIR__.'/auth.php';
