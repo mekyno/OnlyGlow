890e6e17783fd66b3d588042cef04f7a81b28b97
Ondrej Merta <mertaondrej01@gmail.com>
2024-07-16 10:37:34 +0200

    feat: Update dashboard header text to "Admin Panel"

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -17,7 +17,7 @@ const props = defineProps({
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Admin Panel</h2>
         </template>
 
         <div class="py-12">
