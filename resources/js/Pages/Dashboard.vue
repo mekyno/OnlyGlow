7c2b060cfd1bd4fc27b05089a515b1aaaa1a6710
Ondrej Merta <mertaondrej01@gmail.com>
2024-07-16 10:20:25 +0200

    Výpis memberů a jejich kategorií v admin panelu

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,15 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+
+import { defineProps } from 'vue';
+
+// Definice props
+const props = defineProps({
+  members: Object,
+  member_categories: Object,
+});
+
 </script>
 
 <template>
@@ -15,9 +24,22 @@ import { Head } from '@inertiajs/vue3';
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">You're logged in!</div>
-                    <h1>Ahoj</h1>
+                    
+                    <div>
+                        
+                    <div v-for="(category, name) in member_categories" :key="name" class="category">
+                        <h3>{{ name }}</h3>
+                        <ul>
+                            <li v-for="member in category.members" :key="member.id">
+                                {{ member.name }} - {{ member.email }}
+                            </li>
+                        </ul>
+                    </div>
+                    </div>
+
                 </div>
             </div>
         </div>
     </AuthenticatedLayout>
 </template>
+
